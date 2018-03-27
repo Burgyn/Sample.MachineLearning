020af8e760de0df3a0e38ba97140950f250a88a6
Miňo Martiniak <mino.martiniak@gmail.com>
2018-03-27 10:14:56 +0200

    Starting

---
+++ CenkrosTvPrediction/K4Extractor/K4Extractor/K4Extractor/Program.cs
@@ -0,0 +1,54 @@
+﻿using Kros.KORM;
+using Kros.KORM.Query.MsAccess;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K4Extractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MsAccessQueryProviderFactory.Register();
+            var con = @"TopSecret";
+            var count = 0;
+            using (var korm = new Database(con, "System.Data.OleDb"))
+            using (var writer = new StreamWriter("data.csv"))
+            using (var testWriter = new StreamWriter("testing.csv"))
+            {
+                writer.WriteLine("TV;Description");
+                testWriter.WriteLine("TV;Description");
+                foreach (var item in korm.Query<Polozky>()
+                        .Where(p => p.TypVety == "K" || p.TypVety == "M")
+                        .OrderBy(p => p.PopisUplny)
+                        .AsEnumerable()
+                        .Select(p => $"{p.TypVety};{p.PopisUplny.Replace(";", string.Empty)}"))
+                {
+                    count++;
+                    if (count % 1000 == 0)
+                    {
+                        testWriter.WriteLine(item);
+                    }
+                    else
+                    {
+                        writer.WriteLine(item);
+                    }
+                }
+
+                writer.Flush();
+            }
+        }
+
+        public class Polozky
+        {
+            public string TypVety { get; set; }
+
+            public string PopisUplny { get; set; }
+        }
+
+    }
+}
---
+++ CenkrosTvPrediction/K4Extractor/ProgramOld.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Kros.KORM;
+using Kros.KORM.Query.MsAccess;
+
+namespace K4Extractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MsAccessQueryProviderFactory.Register();
+            var con = @"Provider=Microsoft.ACE.OLEDB.12.0;OLE DB Services=-4;Data Source='D:\K4Data\Databazy\ÚRS PRAHA 2018 01.KD';User ID=why;Password=' ';Jet OLEDB:System database=D:\K4Data\System.mdw;Mode=Share Deny None;Locale Identifier=1029;Jet OLEDB:Database Password="";Jet OLEDB:Database Locking Mode=1;Jet OLEDB:Engine Type=5;Persist Security Info=TRUE;";
+
+            using(var korm = new Database(con, "System.Data.OleDb"))
+            using(var writer = new StreamWriter("data.csv"))
+            {
+                foreach (var item in korm.Query<Polozka>()
+                        .Where(p => p.TypVety == "K" || p.TypVety == "M")
+                        .Select(p => $"{p.TypVety};{p.PopisUplny}"))
+                {
+                    writer.WriteLine(item);
+                }
+
+                writer.Flush();
+            }
+        }
+
+        public class Polozka
+        {
+            public string TypVety { get; set; }
+
+            public string PopisUplny { get; set; }
+        }
+    }
+}
